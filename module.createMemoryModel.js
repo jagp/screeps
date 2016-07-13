318ea9b2a38ecafd00ea1590269f54798484b44f
Jared Parmenter <jared.parmenter@gmail.com>
2016-07-13 00:50:37 -0700

    debug

--- module.createMemoryModel.js
+++ module.createMemoryModel.js
@@ -77,7 +77,7 @@ module.exports = function() {
         var sourcesFound = room.find(FIND_SOURCES);
         sourcesFound.forEach(
             function(source) {
-                Memory.colony.rooms[ source.id] ] = { };
+                Memory.colony.rooms[ source.id ] = { };
             }
         );
     }
